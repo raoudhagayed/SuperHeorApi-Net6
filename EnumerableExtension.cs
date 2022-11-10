c52c98926f9a82af3685f6f4d0a5fd2dbf34c312
raoudhagayed <raoudhaelgayed@gmail.com>
2022-11-10 11:02:50 +0100

    Added FirstBranch

---
+++ EnumerableExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHero
+{
+    public static class EnumerableExtension
+    {
+    
+        public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> items, Func<T, TKey> property)
+        {
+            return items.GroupBy(property).Select(x => x.First());
+        }
+    }
+}
---
+++ derivedHero.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Reflection;
+
+namespace SuperHero
+{
+    public class derivedHero : SuperHero
+    {
+        public bool IsAcceccible()
+        {
+            SuperHero b = new SuperHero();
+            bool minfo = b.GetType().GetMethod("MyMethod", BindingFlags.Instance | BindingFlags.NonPublic).IsFamily;
+
+
+            return minfo;
+
+        }
+    }
+}
